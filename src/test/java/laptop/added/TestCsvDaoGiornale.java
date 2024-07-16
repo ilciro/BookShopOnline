172559438c70fa84e3cb37ab61e701324038965d
ilciro <danielecinus10@gmail.com>
2024-07-16 12:07:31 +0200

    fix code smells v5

---
+++ src/test/java/laptop/added/TestCsvDaoGiornale.java
@@ -0,0 +1,30 @@
+package laptop.added;
+
+import com.opencsv.exceptions.CsvValidationException;
+import laptop.database.CsvGiornaleDao;
+import laptop.model.raccolta.Giornale;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+public class TestCsvDaoGiornale {
+    private static final String LOCATION="report/reportGiornali.csv";
+    private  final CsvGiornaleDao csv= new CsvGiornaleDao();
+    private static final Giornale g=new Giornale();
+
+
+    public TestCsvDaoGiornale() throws IOException {
+    }
+
+    @Test
+    void testGiornaleByIdAutoreTitolo() throws CsvValidationException, IOException {
+        g.setTitolo("La Republica1");
+        g.setEditore("La Republica");
+        g.setId(1);
+        assertNotNull(csv.giornaliByIdTitoloEd(new File(LOCATION),g.getTitolo(),g.getEditore()));
+    }
+}
