d202acdc080cde26d26f40c872709026833a27bb
ilciro <danielecinus10@gmail.com>
2024-08-08 20:14:11 +0200

    clean repo

--- src/main/java/laptop/exception/PersistenzaException.java
+++
@@ -1,9 +0,0 @@
-package laptop.exception;
-
-public class PersistenzaException extends Exception{
-
-    public PersistenzaException()
-    {
-        super(" type of persistency not correct !!");
-    }
-}
